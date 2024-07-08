cbfd6007230b1e44c517342e283a2e63680adcf2
kkometan <kkometan@plmj.jp>
2024-07-08 12:59:39 +0900

    WindowsFormsApp1

--- WindowsForms/WindowsFormsApp1/WindowsFormsApp1/CDictionary.cs
+++ WindowsForms/WindowsFormsApp1/WindowsFormsApp1/CDictionary.cs
@@ -17,7 +17,15 @@ namespace WindowsFormsApp1
         private static void GetDictionary()
         {
             var dic = new Dictionary<(int id, int subId), (string s, int n)>();
-            dic.Add((1, 2), ("str", 0));
+            dic.Add((1, 1), ("str11", 11));
+            dic.Add((1, 2), ("str12", 12));
+            dic.Add((1, 3), ("str13", 13));
+            dic.Add((2, 1), ("str21", 21));
+            dic.Add((2, 2), ("str22", 22));
+            dic.Add((2, 3), ("str23", 23));
+
+            // データ抽出
+            (string s, int n) data = dic[(2, 2)];
         }
     }
 }
--- WindowsForms/WindowsFormsApp1/WindowsFormsApp1/CLinq.cs
+++ WindowsForms/WindowsFormsApp1/WindowsFormsApp1/CLinq.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Collections;
 
 namespace WindowsFormsApp1
 {
@@ -19,16 +20,34 @@ namespace WindowsFormsApp1
 
         private static void GetLinq1()
         {
-            var list = new List<int> { 1, 84, 95, 95, 40, 6 };
+            //var list = new List<int> { 1, 84, 95, 95, 40, 6 };
+            var list = new List<int> { 6, 95, 95, 84 ,1 ,40 };
 
+            // クエリ式
             var query = from x in list
                         where x % 2 == 0
                         orderby x
                         select x * 3;
 
+            // ラムダ式
+            var query2 = list.Where(x => x % 2 == 0).Select(x => x * 3).OrderBy(x => x);
+
+            // foreach
+            List<int> result = new List<int>();
+            foreach (var x in list)
+            {
+                if ((x % 2) != 0) continue;
+                int data = x * 3;
+                var y = result.Select((v, i) => new { data = v, index = i }).Where(v => data <= v.data).FirstOrDefault();
+                if (y != null) result.Insert(y.index, data);
+                else result.Add(data);
+            }
+
             Debug.WriteLine("");
             Debug.WriteLine("GetLinq1");
-            Debug.WriteLine(CCommon<int>.GetStrJoin(query.ToList()));
+            Debug.WriteLine("クエリ式：" + CCommon<int>.GetStrJoin(query.ToList()));
+            Debug.WriteLine("ラムダ式：" + CCommon<int>.GetStrJoin(query2.ToList()));
+            Debug.WriteLine("foreach：" + CCommon<int>.GetStrJoin(result));
         }
 
         private static void GetLinq2()
@@ -37,21 +56,16 @@ namespace WindowsFormsApp1
 
             Debug.WriteLine("");
             Debug.WriteLine("GetLinq2");
+            
+            Debug.WriteLine("Max: " + list.Max());          // list の最大値を取得
+            Debug.WriteLine("Min: " + list.Min());          // list の最小値を取得
+            Debug.WriteLine("Average: " + list.Average());  // list の平均値を取得
+            Debug.WriteLine("Sum: " + list.Sum());          // list の合計値を取得
+            Debug.WriteLine("Count: " + list.Count());      // list の要素値を取得
 
-            // list の最大値を取得
-            Debug.WriteLine("Max: " + list.Max());
-
-            // list の最小値を取得
-            Debug.WriteLine("Min: " + list.Min());
-
-            // list の平均値を取得
-            Debug.WriteLine("Average: " + list.Average());
-
-            // list の合計値を取得
-            Debug.WriteLine("Sum: " + list.Sum());
-
-            // list の要素値を取得
-            Debug.WriteLine("Count: " + list.Count());
+            Debug.WriteLine("All: " + list.All(x => x < 100));      // list のすべての要素が 100 未満かどうか
+            Debug.WriteLine("Any: " + list.Any(x => x < 0));        // list のいずれかの要素が 0 未満かどうか
+            Debug.WriteLine("Contains: " + list.Contains(40));      // list に値が 40 の要素が含まれてるかどうか
         }
 
         private static void GetLinq3()
@@ -61,14 +75,6 @@ namespace WindowsFormsApp1
             Debug.WriteLine("");
             Debug.WriteLine("GetLinq3");
 
-            // list のすべての要素が 100 未満かどうか
-            Console.WriteLine("All: " + list.All(x => x < 100));
-
-            // list のいずれかの要素が 0 未満かどうか
-            Console.WriteLine("Any: " + list.Any(x => x < 0));
-
-            // list に値が 40 の要素が含まれてるかどうか
-            Console.WriteLine("Contains: " + list.Contains(40));
         }
 
         private static void GetLinq4()
